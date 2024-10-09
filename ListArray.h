1b29761a1a7d93293afecb9b52f61cd34060d9ec
David Bernad López <dberlop@etsinf.upv.es>
2024-10-09 18:03:31 +0200

    ListArray.h funciona perfectamente y añadimos Makefile y testListArray.cpp

--- ListArray.h
+++ ListArray.h
@@ -9,7 +9,7 @@ template <typename T> class ListArray : public List<T>{
 		T* arr; 						// Puntero al inicio del array que almacenará los elementos de la lista de forma contigua, siendo los elementos de tipo T genérico
 		int max; 						// Tamaño actual del array: no le ponemos const int max porque el tamaño se podrá alterar durante la vida de la lista
 		int n;							// nº de elementos que contiene la lista
-		static const int MINSIZE;		// Tamaño mínimo del array que debe inicializarse a 2 (en el constructor)
+		static const int MINSIZE = 2;	// Tamaño mínimo del array que debe inicializarse a 2 (en el constructor)
 
 		/* Redimensiona el array al tamaño especificado (new_size), para incrementar su capacidad(si está lleno)
 		 o para reducirla (si está demasiado vacío)*/
@@ -18,7 +18,7 @@ template <typename T> class ListArray : public List<T>{
 			T *arr_new = new T[new_size];
 
 			// 2) Copiar el contenido del array actual (arr) en el nuevo array
-			for(int i = 0; i < new_size; i++)
+			for(int i = 0; i < n; i++)
 				arr_new[i] = arr[i];
 
 			// 3) Liberar los recursos de memoria que ocupa arr (usar delete[])
@@ -34,9 +34,9 @@ template <typename T> class ListArray : public List<T>{
 	public:
 		// Constructor de la clase
 		ListArray(){
-			MINSIZE = 2;
-			n = 4;
-			max = 8;
+			MINSIZE;
+			n = 0;
+			max = MINSIZE;
 			arr = new T[MINSIZE];		// Reserva de memoria dinámica para crear un array de tamaño MINSIZE de elementos de tipo T
 		}
 
@@ -47,86 +47,92 @@ template <typename T> class ListArray : public List<T>{
 		
 		// Sobrecarga de operador especial []
 		T operator[](int pos){
-			if(pos >= 0 && pos < max)	// Correcto si el elemento está dentro del rango [0, size()-1)]
+			if(pos >= 0 && pos < n)	// Correcto si el elemento está dentro del rango [0, size()-1)]
 				return arr[pos];		// Devuelve el elemento situado en la posición pos
 			else
 				throw std::out_of_range("Fuera del rango del array");
 		}
 		
 		// Sobrecarga global del operador << (std::cout)
-		friend std::ostream& operator<<(std::ostream& out, const ListArray<T> &list){
+		friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list){
 			
 			// Imprimir una instancia de ListArray<T> por pantalla
-			out << list.arr;
+			if(list.n == 0)
+				out << "List => []";
+			else{
+				out << "List => [";
+				out << "\n";
+				for (int i = 0; i < list.n; i++){
+					out << "  ";
+					out << list.arr[i];
+					if(i != list.n -1){		// Si la posición del array i es distinto del ultimo elemento actual del array
+						out << "\n";
+					}
+				}
+				out << "\n";
+				out << "]";
+			}
+
 			return out;
 		}
 
 		/******* Implementación de los metodos públicos heredados de la clase base List<T> *******/	
 		
 		// Inserta el elemento e en la posición pos del array
+		// Es necesario desplazar todos los elementos a la derecha de esa posición 1 casilla hacia la derecha para hacer espacio
 		void insert(int pos, T e) override{
-			if(pos >= 0 && pos < max)
+			if(pos >= 0 && pos <= n){
+				if(n == max){
+					resize(2 * max);
+				}
+	
+				for(int i = n; i > pos; i--){
+					arr[i] = arr[i-1];		
+				}
+				
+				// Asignación del elemento tras dejar hueco en el array
 				arr[pos] = e;
+				n++;
+			}
 			else
 				throw std::out_of_range("Fuera del rango del array");
 		}
 
 		// Inserta el elemento e al final de la lista
-		void append(T e) override{
-			
-			// 1) Verificar si el array está lleno. Si lo está, llamamos a resize() y trabajamos con un nuevo array más grande
-			if(n == max){
-				int new_size = 2 * max;		// Lo que se hace de normal es duplicar el tamaño máximo del array para reducir el nº de redimensionamientos futuros.
-				resize(new_size);
-			}
-
-			// 2) Insertar un nuevo elemento -> insertar el elemento e en la posición arr[n], ya que n es el nº de elementos de la lista, y el úlimo elemento
-			// de esta representa su tamaño
-			insert(n, e);
-			
-			// 3) Como hemos agregado un nuevo elemento, hay que actualizar en uno el tamaño del array
-			n++;
-			
-		}
+		void append(T e) override {
+		
+			insert(n, e);  // Usa insert para agregar al final
+		}			
+		
 		
 		// Inserta el elemento e al principio de la lista
 		void prepend(T e) override{
-			
-			// 1) Verificar si el array está lleno. Si lo está, llamamos a resize() y trabajamos con un nuevo array más grande
-			if(n == max){
-				int new_size = 2 * max;		// Lo que se hace de normal es duplicar el tamaño máximo del array para reducir el nº de redimensionamientos futuros.
-				resize(new_size);
-			}
 
-			// 2) Desplazar todos los elementos actuales UNA POSICIÓN a la derecha	
-			for(int i = 0; i < n-1; i++){
-				arr[i+1] = arr[i];
-			}
-
-			// 3) Como ahora la posición 0 está libre tras desplazar todos los elementos del array una posición a la derecha, insertamos en ese hueco ele elemento e
 			insert(0, e);
-			n++;
 			
 		}
 		
 		// Elimina y devuelve el elemento situado en la posición pos.
 		// Lanza una excepción std::out_of_range si la posición no es válida
 		T remove(int pos) override{
-			if(pos >= 0 && pos < max){
-				// Desplazamos los elementos situados a la derecha del elemento a eliminar (pos) 1 posición a la izquierda, para así mantener la contigüidad del array
-				arr[pos] = arr[pos+1];
-				return arr[pos];
+			if(pos >= 0 && pos < n){
+				// Guardamos el valor que será eliminado y es de tipo T
+				T valor_eliminado = arr[pos];
+				// Desplazamos TODOS  los elementos situados a la derecha del elemento a eliminar (pos) 1 posición a la izquierda, para así mantener la contigüidad del array
+				for(int i = pos; i < n-1; i++){
+					arr[i] = arr[i+1];
+				}
+			n--;
+			return valor_eliminado;
 			} else
 				throw std::out_of_range("Fuera del rango del array");
 
 		}
 	
-
 		// Devuelve el elemento situado en la posición pos.
 		// Lanza una excepción std::out_of_range si la posición no es válida
 		T get(int pos) override{
-			if(pos >= 0 && pos < max){
-				// Desplazamos los elementos situados a la derecha del elemento a eliminar (pos) 1 posición a la izquierda, para así mantener la contigüidad del array
+			if(pos >= 0 && pos < n){
 				return arr[pos];
 			} else
 				throw std::out_of_range("Fuera del rango del array");
